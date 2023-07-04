0d45565928b154dd86eec04b9ae1127d5c57f287
Briggitte Merchan <briggitte>
2023-07-04 16:13:42 -0400

    what we do section done

--- app/components/WhatWeDo.jsx
+++ app/components/WhatWeDo.jsx
@@ -1,25 +1,11 @@
-import React from 'react'
-import { whatWeDo } from '../constants/whatWeDo'
-import Image from 'next/image'
+import { CardWhat } from "../ui/CardWhat";
 
 export const WhatWeDo = () => {
-  return (
-    <section>
-      <div>
-        <h1>WHAT WE DO</h1>
-      </div>
-      <div className='flex justify-center items-start flex-shrink-0 gap-5 w-[985px] h-[753px]'>
-            {
-              whatWeDo.map(context => (
-                <div key={context.id} className='w-[315px] h-[743px] flex-shrink-0 rounded-[30px] bg-color-card flex flex-col items-center gap-5'>
-                    <Image src={context.imgEvent} alt={context.event} width={290} height={297} className='pt-4'/>
-                    <h3 className='text-[#fff] uppercase font-bebas_neue font-bold leading-[30px] text-center text-base'>{context.event}</h3>
-                    <p className='text-[#C1C2C6] flex font-inter leading-[30px] text-center font-normal text-xs px-7'>{context.content}</p>
-                    <button className='text-sm font-inter text-white w-[250px] h-[50px] rounded-[50px] btn-gradient'>{context.btnInfo}</button>
-                </div>
-              ))
-            }
-      </div>
-    </section>
-  )
-}
+    return (
+        <section className="flex flex-col gap-10 overflow-hidden max-w-[1440px] w-[90%] m-auto">
+                <h1 className="text-[#D9D9D9] text-4xl font-bebas_neue text-center font-bold pt-6">WHAT WE DO</h1>
+                <CardWhat />
+            
+        </section>
+    );
+};
---
+++ app/ui/CardWhat.jsx
@@ -0,0 +1,34 @@
+import { whatWeDo } from "../constants/whatWeDo";
+import Image from "next/image";
+
+export const CardWhat = () => {
+    return (
+        <div className="flex gap-5 items-center overflow-x-scroll sm:overflow-hidden sm:flex-wrap sm:justify-center">
+            {whatWeDo.map((context) => (
+                <div
+                    key={context.id}
+                    className="rounded-[30px] bg-color-card flex flex-col items-center w-[315px] flex-shrink-0 sm:w-[555px]"
+                >
+                    <div className="flex flex-col items-center gap-5 py-4 sm:gap-10 sm:pb-10">
+                        <Image
+                            src={context.imgEvent}
+                            alt={context.event}
+                            width={290}
+                            height={297}
+                            className="sm:w-[503px] sm:h-[297px]"
+                        />
+                        <h3 className="text-[#fff] uppercase font-bebas_neue font-bold leading-[30px] text-center text-base">
+                            {context.event}
+                        </h3>
+                        <p className="text-[#C1C2C6] flex font-inter leading-[30px] text-center font-normal text-xs px-7">
+                            {context.content}
+                        </p>
+                        <button className="text-sm font-inter text-white w-[250px] h-[50px] rounded-[50px] btn-gradient">
+                            {context.btnInfo}
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
